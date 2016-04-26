e562981506b578b7e267e4d3b5feecb99b299f16
OKUNOKENTARO <okunokentaro@me.com>
2016-04-27 00:44:09 +0900

    implement base of add entity

--- app/app-database.ts
+++ app/app-database.ts
@@ -2,9 +2,13 @@ import Dexie from 'dexie';
 
 import {APP_NAME} from './constants';
 
+interface RawEntity {
+  [key: string]: string;
+}
+
 export class AppDatabase extends Dexie {
 
-  moneyTransactions: Dexie.Table<any, any>;
+  rawEntities: Dexie.Table<RawEntity, any>;
 
   constructor() {
     super(APP_NAME);
@@ -18,7 +22,22 @@ export class AppDatabase extends Dexie {
 
   private defineScheme(): void {
     this.version(1).stores({
-      moneyTransactions: '++id, Type, Date, Note'
+      rawEntities: [
+        '++id',
+        'Type',
+        'Date',
+        'Account',
+        'CurrencyCode',
+        'Amount',
+        'AccountTo',
+        'CurrencyCodeTo',
+        'AmountTo',
+        'Category',
+        'Subcategory',
+        'PayeePayer',
+        'Tag',
+        'Note'
+      ].join(',')
     });
   }
 
--- app/money-transaction-repository.service.ts
+++ app/money-transaction-repository.service.ts
@@ -15,19 +15,29 @@ export class MoneyTransactionRepository {
 
   async store(importedResult: any[]): Promise<any> {
     this.initialize();
-    return await this.db.transaction('rw', this.db.moneyTransactions, () => {
-      importedResult.forEach((item) => {
-        this.db.moneyTransactions.add({
-          Type: item.Type,
-          Date: item.Date,
-          Note: item.Note
+    return await this.db.transaction('rw', this.db.rawEntities, () => {
+      importedResult.forEach((item: any) => {
+        this.db.rawEntities.add({
+          'Type'          : item['Type'],
+          'Date'          : item['Date'],
+          'Account'       : item['Account'],
+          'CurrencyCode'  : item['CurrencyCode'],
+          'Amount'        : item['Amount'],
+          'AccountTo'     : item['AccountTo'],
+          'CurrencyCodeTo': item['CurrencyCodeTo'],
+          'AmountTo'      : item['AmountTo'],
+          'Category'      : item['Category'],
+          'Subcategory'   : item['Subcategory'],
+          'PayeePayer'    : item['Payee/Payer'],
+          'Tag'           : item['Tag'],
+          'Note'          : item['Note']
         });
       });
     });
   }
 
   async pull(): Promise<any> {
-    return await this.db.moneyTransactions.toArray();
+    return await this.db.rawEntities.toArray();
   }
 
   private initialize(): void {
