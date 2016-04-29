9651b58074ab9d9707ec53d485c0d892b1bb22d7
OKUNOKENTARO <okunokentaro@me.com>
2016-04-29 23:13:39 +0900

    add a new route MoneyTransactionDetailComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,18 @@ import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from 'angular2/router'
 import {View} from './walts-proto';
 
 import {MoneyTransactionsComponent} from './money-transactions.component';
+import {MoneyTransactionDetailComponent} from './money-transaction-detail.component';
 import {ImportDataComponent} from './import-data.component';
 import {NavComponent} from './nav.component';
 
 import {ToMoneyTransactionsAction} from './actions/to-money-transactions.action';
+import {ToMoneyTransactionDetailAction} from './actions/to-money-transaction-detail.action';
 import {ToImportDataAction} from './actions/to-import-data.action';
 import {AppDispatcher} from './app.dispatcher';
 import {AppStore, AppState} from './app.store';
 import {RouteChanger} from './route-changer.service';
 import {AppDatabaseProvider} from './app-database-provider.service';
+import {MoneyTransactionRepository} from './domain/application/money-transaction/money-transaction-repository.service';
 
 @Component({
   selector  : 'rw-app',
@@ -20,9 +23,11 @@ import {AppDatabaseProvider} from './app-database-provider.service';
     ROUTER_PROVIDERS,
     AppDispatcher,
     AppStore,
+    MoneyTransactionRepository,
     RouteChanger,
     AppDatabaseProvider,
     ToMoneyTransactionsAction,
+    ToMoneyTransactionDetailAction,
     ToImportDataAction
   ],
   template  : `
@@ -36,6 +41,11 @@ import {AppDatabaseProvider} from './app-database-provider.service';
     path        : '/money-transactions',
     name        : MoneyTransactionsComponent.routeName,
     component   : MoneyTransactionsComponent
+  },
+  {
+    path        : '/money-transaction-detail/:id',
+    name        : MoneyTransactionDetailComponent.routeName,
+    component   : MoneyTransactionDetailComponent
   },
   {
     path        : '/import',
@@ -52,4 +62,3 @@ export class AppComponent extends View<AppDispatcher, AppStore, AppState> {
   }
 
 }
-
